9941d16af80cfdd88b0c8c7bbe218b1abbc29cc0
Alexey Elizarov <felex-ae@ya.ru>
2023-10-25 12:13:57 +0000

    fix: deploy

--- src/indexer.js
+++ src/indexer.js
@@ -1,34 +1,34 @@
-const glob = require('@actions/glob');
-const fs = require('fs');
-const path = require('path');
+const glob = require('@actions/glob')
+const fs = require('fs')
+const path = require('path')
 
 async function indexer() {
   // Use glob to find all metadata.json files in subdirectories
-  const globber = await glob.create("**/metadata.json");
+  const globber = await glob.create('**/metadata.json')
 
-  const aggregatedData = [];
+  const aggregatedData = []
 
   for await (const file of globber.globGenerator()) {
     // Read the content of each metadata.json file
-    const fileContent = fs.readFileSync(file, "utf8");
+    const fileContent = fs.readFileSync(file, 'utf8')
     try {
-      const jsonData = JSON.parse(fileContent);
-      aggregatedData.push(jsonData);
+      const jsonData = JSON.parse(fileContent)
+      aggregatedData.push(jsonData)
     } catch (err) {
-      console.error(`Error parsing JSON in ${file}: ${err.message}`);
+      console.error(`Error parsing JSON in ${file}: ${err.message}`)
     }
   }
 
-  const indexFolder = path.join(process.cwd(), "index");
+  const indexFolder = path.join(process.cwd(), 'index')
   if (!fs.existsSync(indexFolder)) {
-    fs.mkdirSync(indexFolder);
+    fs.mkdirSync(indexFolder)
   }
 
   // Define the path to the index.json file within the index folder
-  const indexPath = path.join(indexFolder, "index.json");
+  const indexPath = path.join(indexFolder, 'index.json')
 
   // Write the aggregated data to the index.json file
-  fs.writeFileSync(indexPath, JSON.stringify(aggregatedData, null, 2));
+  fs.writeFileSync(indexPath, JSON.stringify(aggregatedData, null, 2))
 }
 
 module.exports = { indexer }
